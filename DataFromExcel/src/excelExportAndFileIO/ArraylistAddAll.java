db190c60e56562a1cb63b9fe3665cb7ff7e211c8
Bhavik <Bhavik@DESKTOP-2PFO99T>
2017-11-30 18:06:33 +0530

    test

--- DataFromExcel/src/excelExportAndFileIO/ArraylistAddAll.java
+++ DataFromExcel/src/excelExportAndFileIO/ArraylistAddAll.java
@@ -6,11 +6,13 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import org.testng.Assert;
+
 public class ArraylistAddAll {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<String> test1 = new ArrayList<String>();
+		/*List<String> test1 = new ArrayList<String>();
 		test1.add("ramu");
 		test1.add("samu");
 		test1.add("tamu");
@@ -34,7 +36,39 @@ public class ArraylistAddAll {
         System.out.println(retainThese);
         
         List<String> retainThese1 = Arrays.asList("Colorado", "New York");
-        System.out.println(retainThese1);
+        System.out.println(retainThese1);*/
+        
+       /* String test = "abcd";
+        String rtest = "";
+        String temp="";
+        char[] ctest= test.toCharArray();
+        for(int i=ctest.length-1;i>=0;i--)
+        {
+        	temp = rtest+Character.toString(ctest[i]);
+        	rtest = temp;
+        }
+		System.out.println(rtest);
+		String sbtest = new StringBuilder(test).reverse().toString();
+		Assert.assertEquals(rtest, sbtest);
+		*/
+		String a = "bhavik123";
+		String r = "";
+		List<Integer> num = new ArrayList<Integer>();
+		for ( int i = a.length() - 1; i >= 0; i-- ) 
+		{
+			  /*r = r + a.charAt(i);*/
+			r=""+a.charAt(i);
+			  try{
+				  num.add(Integer.parseInt(r));
+			  }
+			  catch (Exception e) {
+				// TODO: handle exception
+			}
+			  
+		}
+	       
+		System.out.println(r);
+		System.out.println(num);
 		
 	}
 
